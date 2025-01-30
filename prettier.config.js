cd644ad6fe873cc27b2d886439287d6ee6827fd7
Josh Vickery <jcv8000@gmail.com>
2025-01-30 02:14:03 -0600

    nwe prettier config

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+    tabWidth: 4,
+    trailingComma: "none",
+    printWidth: 100,
+    endOfLine: "lf"
+};
+
+export default config;
