91633ae9df86b92e97d307cd36f0c1c48260e210
Josh Vickery <jcv8000@gmail.com>
2025-01-30 17:56:48 -0600

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import pkg from "./package.json";
 import paths from "vite-tsconfig-paths";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig(() => {
     process.env.VITE_APP_VERSION = pkg.version;
     return {
-        plugins: [react(), paths()],
+        plugins: [paths(), react(), VitePWA({ manifest: { theme_color: "#6741d9" } })],
         build: {
             outDir: "dist/client"
         },
