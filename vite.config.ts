653a4e4feec4cd7768d03c936ebc971b9784cf7c
Josh Vickery <jcv8000@gmail.com>
2025-01-31 18:10:23 -0600

    strict port 5173

--- vite.config.ts
+++ vite.config.ts
@@ -39,7 +39,9 @@ export default defineConfig(() => {
             outDir: "dist/client"
         },
         server: {
-            host: import.meta.env.VITE_DEV_HOST
+            host: import.meta.env.VITE_DEV_HOST,
+            port: 5173,
+            strictPort: true
         }
     };
 });
